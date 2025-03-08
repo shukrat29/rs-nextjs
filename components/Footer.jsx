b881c21215a8785e867b0514b9becd8e5cbfef74
Shukra Tamang <shukratamang.npl@gmail.com>
2025-03-08 23:21:11 +1100

    create infoboxes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,13 @@ import logo from "@/assets/images/logo.png";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer class="bg-gray-200 py-4 mt-24">
-      <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-        <div class="mb-4 md:mb-0">
-          <Image src={logo} alt="Logo" class="h-8 w-auto" />
+    <footer className="bg-gray-200 py-4 mt-24">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+        <div className="mb-4 md:mb-0">
+          <Image src={logo} alt="Logo" className="h-8 w-auto" />
         </div>
-        <div class="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
-          <ul class="flex space-x-4">
+        <div className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
+          <ul className="flex space-x-4">
             <li>
               <Link href="/properties">Properties</Link>
             </li>
@@ -21,7 +21,7 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <p class="text-sm text-gray-500 mt-2 md:mt-0">
+          <p className="text-sm text-gray-500 mt-2 md:mt-0">
             &copy; {currentYear} PropertyPulse. All rights reserved.
           </p>
         </div>
